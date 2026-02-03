2b96cf9c00b1efb6ce6900ca74b0f9aa9c88173c
700014473 <700014473@2260I-L29-008>
2026-02-03 13:20:33 -0500

    Finished

--- Topic 1 - Printing and Comments/Program.cs
+++ Topic 1 - Printing and Comments/Program.cs
@@ -7,29 +7,38 @@ namespace Topic_1___Printing_and_Comments
         static void Main(string[] args)
         {
             // Console.Writeline(); = print()
-
             Console.WriteLine("Hello, World!");
 
             // Colours the text red
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("My name is Angel.");
             Console.WriteLine(" ");
-            
-            // Does not add move the cursor to the next line
 
+            // Console.Write does not add move the cursor to the next line
             Console.Write("Computers");
+
+            // Console.WriteLine adds a new line after printing
             Console.WriteLine(" Rule!");
+
+            Console.WriteLine("Press ENTER to continue...");
+
+            // Pauses the program until the user presses ENTER
+            Console.ReadLine();
+
+            // Colours the background cyan
+            Console.BackgroundColor = ConsoleColor.Cyan;
+
+            // Clears the console to apply the background colour to the whole screen
+            Console.Clear();
+
+            Console.WriteLine("My favourite food is pizza!");
             Console.WriteLine("Press ENTER to continue...");
             Console.ReadLine();
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.Clear();
-            Console.WriteLine("My favourite food is pizza!");
-            Console.WriteLine("Press ENTER to continue...");  
-            Console.ReadLine();
+
+            // Resets the console colours to default
             Console.ResetColor();
+
             // To set a comment, use two forward slashes.
-
-
 
             // Makes a box with a yellow background
             Console.BackgroundColor = ConsoleColor.Yellow;
